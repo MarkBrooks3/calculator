17de77dafa8991ffb99d9e24825d6f3ecc85a8c7
mark <78883486+MarkBrooks3@users.noreply.github.com>
2026-05-08 15:17:57 -0500

    initial commit

---
+++ index.js
@@ -0,0 +1,32 @@
+let numOne;
+let numTwo;
+let operator;
+
+function add(x, y) {
+  return x + y;
+}
+
+function subtract(x, y) {
+  return x - y;
+}
+
+function multiply(x, y) {
+  return x * y;
+}
+
+function divide(x, y) {
+  return x / y;
+}
+
+function operate(operation, x, y) {
+  switch (operation) {
+    case 'add':
+      return add(x, y);
+    case 'subtract':
+      return subtract(x, y);
+    case 'multiply':
+      return multiply(x, y);
+    case 'divide':
+      return divide(x, y);
+  }
+}
